e3a3e37578b72db62a2fcac8794403fa00bb7c8d
Jesus Mireles <jesusmireles43@ymail.com>
2023-01-24 21:19:13 -0600

    Initial Commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+fun main() {
+   val myByte: Byte = 7
+   val myShort: Short = 3
+   val myInt = 77
+    val myLong: Long = 17
+    val myFloat = 6.28F
+    val myDouble = 21.98
+    val myBoolean = true
+    val myChar = 'J'
+
+    println("Byte example: $myByte")
+    println("Short example: $myShort")
+    println("Int example: $myInt")
+    println("Long example: $myLong")
+    println("Float example: $myFloat")
+    println("Double example: $myDouble")
+    println("Boolean example: $myBoolean")
+    println("Char example: $myChar")
+
+}
